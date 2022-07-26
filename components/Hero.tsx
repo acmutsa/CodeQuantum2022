6c5bef20789f103f523419a45e4df03d70bf58d9
Danielle Tobler <danielle.tobler210@gmail.com>
2022-07-26 12:28:44 -0500

    adjustments to hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { createStyles, Overlay, Container, Title, Button, Text } from '@mantine/core';
+import { createStyles, Overlay, Container, Title, Button, Text, Grid } from '@mantine/core';
 
 const useStyles = createStyles((theme) => ({
     hero: {
         position: 'relative',
         // backgroundImage:
-            // 'url(https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)',
+        //     'url(https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
+    },
+
+    outerContainer: {
+        height: 700,
+        maxWidth: '90%',
+        minWidth: 1300,
+        paddingRight: '5rem',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
     },
 
     container: {
@@ -26,11 +37,21 @@ const useStyles = createStyles((theme) => ({
         },
     },
 
+    image: {
+        width: 550,
+        zIndex: 2,
+        [theme.fn.smallerThan('sm')]: {
+            display: 'none',
+        },
+    },
+
     title: {
-        color: theme.white,
+        color: theme.black,
         fontSize: 60,
         fontWeight: 900,
         lineHeight: 1.1,
+        variant: 'gradient',
+        gradient: { from: 'indigo', to: 'cyan', deg: 45 },
 
         [theme.fn.smallerThan('sm')]: {
             fontSize: 40,
@@ -44,7 +65,7 @@ const useStyles = createStyles((theme) => ({
     },
 
     subtitle: {
-        color: theme.white,
+        color: theme.black,
         fontSize: 40,
         fontWeight: 700,
         lineHeight: 1.0,
@@ -62,7 +83,7 @@ const useStyles = createStyles((theme) => ({
     },
 
     boldHeading: {
-        color: theme.white,
+        color: theme.black,
         fontSize: '1.75rem',
         fontWeight: 800,
         lineHeight: 1.0,
@@ -80,7 +101,7 @@ const useStyles = createStyles((theme) => ({
     },
 
     description: {
-        color: theme.white,
+        color: theme.black,
         maxWidth: 600,
 
         [theme.fn.smallerThan('sm')]: {
@@ -103,21 +124,24 @@ export function HeroContentLeft() {
 
     return (
         <div className={classes.hero}>
-            <Overlay
-              gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
-              opacity={1}
-              zIndex={0}
-            />
-            <Container className={classes.container}>
-                <Title className={classes.title} order={1}>CodeQuantum 2022</Title>
-                <Title className={classes.subtitle} order={1}>November 5th - 6th</Title>
-                <Text className={classes.description} size="xl" mt="xl">
-                    <Title className={classes.boldHeading}>What is CodeQuantum?</Title>
-                    CodeQuantum is San Antonio’s first 24-hour hackathon geared towards <a href="https://www.womenforpoliticalchange.org/appendix" rel="external">marginalized genders</a> to promote inclusivity and diversity! Organized by the ACM-W chapter at UTSA, CodeQuantum is dedicated to creating a safe hacking space for students in the tech field.
-                </Text>
-                <Button variant="gradient" size="xl" radius="xl" className={classes.control}>
-                    Register
-                </Button>
+            {/*<Overlay*/}
+            {/*  gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"*/}
+            {/*  opacity={1}*/}
+            {/*  zIndex={0}*/}
+            {/*/>*/}
+            <Container className={classes.outerContainer}>
+                <Container className={classes.container}>
+                    <Title className={classes.title} order={1}>CodeQuantum 2022</Title>
+                    <Title className={classes.subtitle} order={1}>November 5<sup>th</sup> - 6<sup>th</sup></Title>
+                    <Text className={classes.description} size="xl" mt="xl">
+                        <Title className={classes.boldHeading}>What is CodeQuantum?</Title>
+                        CodeQuantum is San Antonio’s first 24-hour hackathon geared towards <a href="https://www.womenforpoliticalchange.org/appendix" rel="external">marginalized genders</a> to promote inclusivity and diversity! Organized by the ACM-W chapter at UTSA, CodeQuantum is dedicated to creating a safe hacking space for students in the tech field.
+                    </Text>
+                    <Button variant="gradient" size="xl" radius="xl" className={classes.control}>
+                        Register
+                    </Button>
+                </Container>
+                <img src="/images/placeholder.jpg" alt="Person at computer" className={classes.image} />
             </Container>
         </div>
     );
