a496a80ae2b0decbdb71c9ed60cf644040c36834
Danielle Tobler <danielle.tobler210@gmail.com>
2022-07-26 11:30:41 -0500

    feat: sponsor section rework

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { Title } from '@mantine/core';
 import { HeaderSimple as Header } from '../components/Header';
 import { HeroContentLeft as Hero } from '../components/Hero';
 import { navLinks as HeaderLinks, footerLinks as FooterLinks } from '../data/_links';
+import { FeaturesGrid as Sponsors } from '../components/Sponsors';
 import { FooterLinks as Footer } from '../components/Footer';
 // import { Section } from '../components/Section';
 // import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
@@ -12,8 +13,7 @@ export default function HomePage() {
     <>
       <Header links={HeaderLinks} />
       <Hero />
-      {/*TODO: Create section component for each, encapsulate Title + Carousel into Section*/}
-      <Title order={3} pt="2rem" pb="2rem" style={{ textAlign: 'center' }}>Special thanks to our Partners: </Title>
+      <Sponsors />
       <Footer data={[]} />
       {/*<ColorSchemeToggle />*/}
     </>
