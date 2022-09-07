9b769ec5fb40252b110246319ce86ec60c7abb55
Lermatroid <liam@murray.to>
2022-09-07 01:59:44 -0500

    Remove ServerSideProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
 import { GetServerSidePropsContext } from 'next';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { AppProps } from 'next/app';
 import { getCookie, setCookies } from 'cookies-next';
 import Head from 'next/head';
 import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
-import theme from '../data/mantineThemeOverride'
+import theme from '../data/mantineThemeOverride';
 
-export default function App(props: AppProps & { colorScheme: ColorScheme }) {
+export default function App(props: AppProps) {
   const { Component, pageProps } = props;
-  const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme);
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
 
   const toggleColorScheme = (value?: ColorScheme) => {
     const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark');
@@ -23,8 +23,14 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
         <title>CodeQuantum 2022</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="shortcut icon" href="/images/CQ_Logo_2022.svg" />
-        <meta name="description" content="CodeQuantum is San Antonio’s first 24-hour hackathon geared towards marginalized genders to promote inclusivity and diversity!" />
-        <meta name="keywords" content="hackathon, coding, computer science, utsa, college, marginalized genders" />
+        <meta
+          name="description"
+          content="CodeQuantum is San Antonio’s first 24-hour hackathon geared towards marginalized genders to promote inclusivity and diversity!"
+        />
+        <meta
+          name="keywords"
+          content="hackathon, coding, computer science, utsa, college, marginalized genders"
+        />
         <meta name="robots" content="index, nofollow" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="English" />
@@ -33,11 +39,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-        <MantineProvider
-          withGlobalStyles
-          withNormalizeCSS
-          theme={theme}
-        >
+        <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
           <NotificationsProvider>
             <Component {...pageProps} />
           </NotificationsProvider>
@@ -46,7 +48,3 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
     </>
   );
 }
-
-App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
-  colorScheme: getCookie('mantine-color-scheme', ctx) || 'light',
-});
